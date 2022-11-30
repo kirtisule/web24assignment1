35d4c61713c72742e1cf1f1985d7c8bc6fbc8849
kirtisule <kirtisule27111996@gmail.com>
2022-11-30 13:47:51 +0530

    assignment questions

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+let rev="";
+let str="madam";
+for(let i=0;i<=str.length-1;i++){
+       rev=rev+str[i];
+    }if(rev==str[i]){
+       console.log("Yes");
+     }else{
+         console.log("No");
+      }
---
+++ prime.js
@@ -0,0 +1,7 @@
+let count=0;
+    let num=11;
+     for(let i=0;i<=num;i++){
+         if(num%i==0){
+           count++;
+          }
+      }console.log(count)
